683673bb6637b382ee2425517c1b255e86d0f63d
ted whalen <tewhalen@gmail.com>
2022-03-25 15:27:39 -0500

    Update auto_pipenv.sh

--- shell_scripts/auto_pipenv.sh
+++ shell_scripts/auto_pipenv.sh
@@ -4,4 +4,8 @@ function auto_pipenv_shell {
     if [ -f "Pipfile" ] ; then
         source "$(pipenv --venv)/bin/activate"
     fi
+    if [ -f "poetry.lock" ] ; then
+        source "$(poetry env info -p)/bin/activate"
+    fi
+}
 }
