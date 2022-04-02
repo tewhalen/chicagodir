3da82f7c2ffed43d1e6c03cb6cecac27f664c3d6
ted whalen <tewhalen@gmail.com>
2022-04-02 12:56:57 -0500

    more defaults

--- chicagodir/settings.py
+++ chicagodir/settings.py
@@ -13,11 +13,11 @@ env.read_env()
 
 ENV = env.str("FLASK_ENV", default="production")
 DEBUG = ENV == "development"
-SQLALCHEMY_DATABASE_URI = env.str("DATABASE_URL").replace(
-    "postgres://", "postgresql://"
-)
+SQLALCHEMY_DATABASE_URI = env.str(
+    "DATABASE_URL", default="sqlite:////tmp/dev.db"
+).replace("postgres://", "postgresql://")
 SECRET_KEY = env.str("SECRET_KEY")
-SEND_FILE_MAX_AGE_DEFAULT = env.int("SEND_FILE_MAX_AGE_DEFAULT")
+SEND_FILE_MAX_AGE_DEFAULT = env.int("SEND_FILE_MAX_AGE_DEFAULT", default=0)
 BCRYPT_LOG_ROUNDS = env.int("BCRYPT_LOG_ROUNDS", default=13)
 DEBUG_TB_ENABLED = DEBUG
 DEBUG_TB_INTERCEPT_REDIRECTS = False
