e312936222e26bc428688ac0da0fef6ee141e07d
ted whalen <tewhalen@gmail.com>
2022-04-18 09:37:49 -0500

    javascript lint? what next?

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,22 @@
 // App initialization code goes here
 
-$(function () {
-
-    $("#street_search").autocomplete({
-        source: "/streets",
-        minLength: 2,
-        focus: function (event, ui) {
-            $("#street_search").val(ui.item.label);
-            return false;
-        },
-        select: function (event, ui) {
-            $("#street_search").val(ui.item.label);
-            $("#street_search-id").val(ui.item.id);
-            return false;
-        }
-    })
-        .autocomplete("instance")._renderItem = function (ul, item) {
-            return $("<li>")
-                .append("<div>" + item.label + "</div>")
-                .appendTo(ul);
-        };
+$(() => {
+  $('#street_search').autocomplete({
+    source: '/streets',
+    minLength: 2,
+    focus(event, ui) {
+      $('#street_search').val(ui.item.label);
+      return false;
+    },
+    select(event, ui) {
+      $('#street_search').val(ui.item.label);
+      $('#street_search-id').val(ui.item.id);
+      return false;
+    },
+  })
+    .autocomplete('instance')._renderItem = function (ul, item) {
+      return $('<li>')
+        .append(`<div>${item.label}</div>`)
+        .appendTo(ul);
+    };
 });
